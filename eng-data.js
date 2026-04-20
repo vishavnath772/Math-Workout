4d0237075e8e5f04f6eaf76f01d7a03fb5b7cbd6
Vishavnath <vishavnathkumar772@gmail.com>
2026-04-20 16:26:44 +0530

    Create eng-data.js

---
+++ eng-data.js
@@ -0,0 +1,25 @@
+// Put all your English questions inside these brackets [ ]
+const ALL_ENG_MCQ = [
+  {
+    id: 5001,
+    q: "Choose the correct Synonym for: 'ABANDON'",
+    a: "Keep",
+    b: "Forsake",
+    c: "Cherish",
+    d: "Hold",
+    ans: "b",
+    topic: "Synonyms",
+    fact: "Abandon means to leave or give up completely."
+  },
+  {
+    id: 5002,
+    q: "Spot the error: 'He don't like apples.'",
+    a: "He",
+    b: "don't",
+    c: "like",
+    d: "apples",
+    ans: "b",
+    topic: "Spot the Error",
+    fact: "With 'He/She/It', we use 'doesn't', not 'don't'."
+  }
+];
